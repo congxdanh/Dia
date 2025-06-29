940f26524dc798224f05f3972b14bee73f2283af
congxdanh <danh.hsb13@gmail.com>
2025-06-29 13:11:07 +0700

    dsa

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,53 +1,53 @@
 import express from 'express';
 import userRoutes from '../routes/user.route';
-import courseRoutes from '../routes/course.route';
-import orderRoutes from '../routes/order.route';
-import notificationRoutes from '../routes/notification.route';
-import layoutRoutes from '../routes/layout.route';
-import categoryRoutes from '../routes/category.route';
-import levelRoutes from '../routes/level.route';
-import quizRoutes from '../routes/quiz.route';
-import progressRoutes from '../routes/progress.route';
-import incomeRoutes from '../routes/income.route';
-import paymentRoutes from '../routes/payment.route';
-import creditCardRoutes from '../routes/creditCard.route';
-import requestRoutes from '../routes/request.route';
-import sectionRoutes from '../routes/section.route';
-import lessonRoutes from '../routes/lesson.route';
-import businessRoutes from '../routes/business.route';
+// import courseRoutes from '../routes/course.route';
+// import orderRoutes from '../routes/order.route';
+// import notificationRoutes from '../routes/notification.route';
+// import layoutRoutes from '../routes/layout.route';
+// import categoryRoutes from '../routes/category.route';
+// import levelRoutes from '../routes/level.route';
+// import quizRoutes from '../routes/quiz.route';
+// import progressRoutes from '../routes/progress.route';
+// import incomeRoutes from '../routes/income.route';
+// import paymentRoutes from '../routes/payment.route';
+// import creditCardRoutes from '../routes/creditCard.route';
+// import requestRoutes from '../routes/request.route';
+// import sectionRoutes from '../routes/section.route';
+// import lessonRoutes from '../routes/lesson.route';
+// import businessRoutes from '../routes/business.route';
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
 
-router.use('/courses', courseRoutes);
+// router.use('/courses', courseRoutes);
 
-router.use('/sections', sectionRoutes);
+// router.use('/sections', sectionRoutes);
 
-router.use('/lessons', lessonRoutes);
+// router.use('/lessons', lessonRoutes);
 
-router.use('/orders', orderRoutes);
+// router.use('/orders', orderRoutes);
 
-router.use('/notifications', notificationRoutes);
+// router.use('/notifications', notificationRoutes);
 
-router.use('/layout', layoutRoutes);
+// router.use('/layout', layoutRoutes);
 
-router.use('/levels', levelRoutes);
+// router.use('/levels', levelRoutes);
 
-router.use('/categories', categoryRoutes);
+// router.use('/categories', categoryRoutes);
 
-router.use('/quizzes', quizRoutes);
+// router.use('/quizzes', quizRoutes);
 
-router.use('/progress', progressRoutes);
+// router.use('/progress', progressRoutes);
 
-router.use('/income', incomeRoutes);
+// router.use('/income', incomeRoutes);
 
-router.use('/payment', paymentRoutes);
+// router.use('/payment', paymentRoutes);
 
-router.use('/credit-cards', creditCardRoutes);
+// router.use('/credit-cards', creditCardRoutes);
 
-router.use('/request', requestRoutes);
+// router.use('/request', requestRoutes);
 
-router.use('/business', businessRoutes);
+// router.use('/business', businessRoutes);
 
 export default router;
